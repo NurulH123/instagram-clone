16cadf4e1e59a62a871c2d6f1a23b0c4a640420d
NurulH123 <hidayatnurul937@gmail.com>
2024-03-20 18:49:11 +0700

    desaign detail berhasil

--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -53,7 +53,55 @@
             </aside>
 
             <aside class="lg:col-span-5 border-l p-3 h-full flex flex-col grap-4 overflow-hidden">
+                <div class="flex items-center gap-2">
+                    <x-avatar class="w-9 h-9" />
+                    <h5 class="font-bold">{{ fake()->name }}</h5>
+                </div>
 
+                <div class="">
+                    <textarea 
+                    placeholder="Add a caption"
+                    class="border-0 focus:border-0 px-0 w-full rounded bg-white h-32 focus:outline-none focus:ring-0"
+                    id=""></textarea>
+                </div>
+
+                <div class="w-full item-center">
+                    <input 
+                    placeholder="Add location"
+                    type="text" 
+                    class="border-0 focus:border-0 px-0 w-full rounded bg-white focus:outline-none focus:ring-0">
+                </div>
+
+                <div class="">
+                    <h6 class="text-gray-500 font-medium text-base">Advanced  Settings</h6>
+
+                    <ul>
+                        <li>
+                            <div class="flex item-center gap-3 justify-between">
+                                <span>Hide like and view counts on this post</span>
+
+                                <label class="inline-flex items-center cursor-pointer">
+                                    <input type="checkbox" value="" class="sr-only peer">
+                                    <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+                                </label>
+  
+                            </div>
+                        </li>
+                        <li>
+                            <div class="flex item-center gap-3 justify-between">
+                                <span>Turn off commenting</span>
+
+                                <label class="inline-flex items-center cursor-pointer">
+                                    <input type="checkbox" value="" class="sr-only peer">
+                                    <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"></span>
+                                </label>
+  
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </aside>
 
         </main>
